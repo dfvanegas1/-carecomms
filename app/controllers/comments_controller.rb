8c2e9d6b15f6e7279d9b4fe612424714e973752b
daniel vanegas <dfvanegas@gmail.com>
2024-03-03 16:06:27 +0100

    task show page basic and comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,23 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
   def create
+    @comment = Comment.new(comment_params)
+    @comment.task_id = params[:task_id]
+    @comment.user_id = current_user.id # Assuming you have a current_user method
+
+    # if @comment.save
+    #   redirect_to task_path(@comment.task_id), notice: 'Comment was successfully added.'
+    # else
+    #   # Handle the case where comment can't be saved
+    #   # This might involve rendering the task's show view again with error messages
+    #   redirect_to task_path(@comment.task_id), alert: 'Error adding comment.'
+    # end
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :task_id)
+  end
+
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,8 +3,17 @@ class TasksController < ApplicationController
   end
 
   def show
+    @task = Task.includes(user_tasks: :user).find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to tasks_path, alert: 'Task not found.'
   end
 
   def update
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :deadline)
   end
 end
