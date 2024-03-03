965da298493b6d75d373563ed5d43e5719ea33d7
daniel vanegas <dfvanegas@gmail.com>
2024-03-03 20:37:02 +0100

    shift and update shift in profile page

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,10 +1,28 @@
+# app/controllers/shifts_controller.rb
 class ShiftsController < ApplicationController
-  def index
-  end
-
-  def edit
-  end
+  before_action :authenticate_user!
+  before_action :set_user, only: [:update]
+  before_action :set_shift, only: [:update]
 
   def update
+    if @shift.update(shift_params)
+      redirect_to profile_path(id: @user.id), notice: 'Shift was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_shift
+    @shift = @user.shifts.find(params[:id])
+  end
+
+  def shift_params
+    params.require(:shift).permit(:start_date, :end_date)
   end
 end
