e8aa5f90cb0a93aa90ca806983e869641b1ca198
Francesca Correa <francescacciavaglia@gmail.com>
2024-03-05 09:05:03 +0100

    update pages controller with dashboard action and update dashboard view with variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+
+  end
+
+  def dashboard
+    @tasks = Task.all
+    @users = User.all
   end
 end
