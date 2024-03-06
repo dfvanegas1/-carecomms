b7e74a0ffa77b7bfe2b7546888c5402ab1eca58b
Francesca Correa <francescacciavaglia@gmail.com>
2024-03-06 14:56:46 +0100

    delete unnecesary actions

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,8 +1,6 @@
 class ShiftsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_shift, only: [:update]
-  before_action :set_user_from_profile, only: [:update]
-  before_action :authorize_shift_update, only: [:update]
 
   def update
     if @shift.update(shift_params)
@@ -26,22 +24,5 @@ class ShiftsController < ApplicationController
 
   def shift_params
     params.require(:shift).permit(:start_date, :end_date)
-  end
-
-  def set_user_from_profile
-    # This sets @user based on the profile being visited
-    @user = User.find(params[:user_id])
-  end
-
-  def authorize_shift_update
-    unless can_edit_shifts_for?(@user) || current_user.admin?
-      redirect_to root_path, alert: "You're not authorized to edit this shift."
-    end
-  end
-
-  def can_edit_shifts_for?(user)
-    # Your custom logic here. This example assumes you might check if the current_user is an admin,
-    # or if the current_user is part of the same team or has specific permissions to edit shifts for the visited user.
-    current_user.admin? || current_user == user || current_user.has_permission?(:edit_shifts, for: user)
   end
 end
